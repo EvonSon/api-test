92e58c7191b0a744fa61b02e4b2f386686b1c04a
EvonSon <sona140@newschool.edu>
2024-11-19 09:33:32 -0500

    Update data.js

--- data.js
+++ data.js
@@ -3,6 +3,7 @@ fetch('https://api.thecatapi.com/v1/images/search?limit=10').then(function (resp
 	return response.json();
 })
 .then(function (data) {
+    appendData(data);
 	// This is the JSON from our response
 	console.log(data);
 
@@ -12,12 +13,11 @@ fetch('https://api.thecatapi.com/v1/images/search?limit=10').then(function (resp
 	console.warn('Something went wrong.', err);
 });
 
-
 function appendData(data) {
     var mainContainer = document.getElementById("myData");
     for (var i = 0; i < data.length; i++) {
         var div = document.createElement("div");
-        div.innerHTML = 'Name: ' + data[i].id + ' pic: ' + data[i].url;
+        div.innerHTML = 'Name: ' + data[i].id + ' pic: ' + data[i].url + `<div class="image-list"><img src ="${data[i].url}"</div>`;
         mainContainer.appendChild(div);
     }
 }
